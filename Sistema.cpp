2d1be809692b025253fafbb8b42b00ec85ed49d3
nicol <ngalleguillos9@gmail.com>
2024-09-25 16:54:23 -0300

    Sistema Añadido jeje

---
+++ Sistema.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nicol on 25-09-2024.
+//
+
+#include "Sistema.h"
---
+++ Sistema.h
@@ -0,0 +1,16 @@
+//
+// Created by nicol on 25-09-2024.
+//
+
+#ifndef SISTEMA_H
+#define SISTEMA_H
+
+
+
+class Sistema {
+
+};
+
+
+
+#endif //SISTEMA_H
